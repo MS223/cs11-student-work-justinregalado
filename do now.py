dea9741f3b13df404b68b987a868f1eff385ee2c
justinregalado <justin.regalado@ms223.org>
2016-03-21 08:39:36 -0400

    Create do now.py

---
+++ do now.py
@@ -0,0 +1,12 @@
+import random
+random.randint(0, 3)
+random.randint(0, 3)
+print(random.randint(3, 9))
+print(random.randint(1, 14))
+print(random.randint(2, 45))
+
+#it went from 3 to 1 to 0 thats all i seen that changed #
+# when you keep running it it changes
+#they dont ever pass 3
+# the first number cant be bigger than the last number
+#each line is different because of the number imput 
