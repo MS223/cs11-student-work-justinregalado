3d1d2bcbff3f6a133c00b19b4e630d979bb04cbc
bensamuelskalow@gmail.com <bensamuelskalow@gmail.com>
2016-03-22 09:38:09 -0400

    Update fizz buzz.py

--- fizz buzz.py
+++ fizz buzz.py
@@ -1,9 +1,11 @@
 for groot in range(1,101):
-    if groot % 3 == 0:
+    if groot % 3 == 0: #checking for multiples of 3
         print('fizz')
-    elif groot % 5 == 0:
+    elif groot % 5 == 0: # checking for multiples of 5
         print('buzz')
-    elif groot % 3 == 0 and groot % 5 == 0:
+    elif groot % 3 == 0 and groot % 5 == 0: #checking for multiples of 3 AND 5
         print('fizzbuzz')
     else:
         print groot
+
+# Why isn't the code finding the multiples of 3 AND 5? What is it checking for first?
