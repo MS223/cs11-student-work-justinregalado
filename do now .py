15ffda54040af1c883ea715b7ac0c302e9a67e31
justinregalado <justin.regalado@ms223.org>
2016-04-01 08:28:51 -0400

    do now 14/1/16.py

---
+++ do now .py
@@ -0,0 +1,26 @@
+# For each example below, predict what will be printed. Then, run the code and confirm your prediction.
+
+# a = 0
+# while a< 100:
+# 	print a
+"""
+Prediction: # i think it will only print A
+Observation: it was a while loop
+ """
+
+# a = 0
+# while a < 100:
+# 	a = a + 1
+# 	print a
+# """
+# Prediction: #this will stop some where becuase of the a = a+1
+# Observation:# this is a whole loop that had an ending
+#  """
+
+a = raw_input("Would you like to quit: ")
+while a != "y":
+	a = raw_input("Would you like to quit: ")
+"""
+Prediction:#i think it will keep asking me till i put "y"
+Observation: it did what i predicted which was "y" it didnt have the "n"
+ """
