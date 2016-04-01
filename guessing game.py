ecea0adaf802c7bd53096e9a9aeb52f7fcb895fd
justinregalado <justin.regalado@ms223.org>
2016-04-01 08:42:06 -0400

    Create guessing game.py

---
+++ guessing game.py
@@ -0,0 +1,6 @@
+a=raw_input("whats your name ")
+a=raw_input("pick a number!")
+from random import randint
+random=(randint(1,80))
+while a != random
+    #need to ask the quesions about low and high
