7c53c65529ea9c40bf5a34e472f194d17fe14ad5
justinregalado <justin.regalado@ms223.org>
2016-04-04 08:42:05 -0400

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,6 +1,12 @@
-a=raw_input("whats your name ")
-a=raw_input("pick a number!")
+user=raw_input("whats your name? ")
+guess=input('whats the biggest number you want to  guess?')
+number=input("pick a number!")
 from random import randint
-random=(randint(1,80))
-while a != random
-    #need to ask the quesions about low and high
+random=(randint(1,number))
+answer=
+while guess != number:
+    if guess < number:
+        print "too high"
+    elif guess > number:
+        print 'too low'
+        #i must check what i will be putting in for answer
