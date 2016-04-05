140aa5d4d4f7e751daa8581fd7905e0660817bdf
justinregalado <justin.regalado@ms223.org>
2016-04-05 09:28:16 -0400

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,18 +1,18 @@
 guesses=1
-user=raw_input("whats your name? ")
-number=input('whats the biggest number you want to  guess?')
-guess=input("pick a number!")
+user=raw_input("whats your name? ")#askes you whats your name
+number=input('whats the biggest number you want to  guess?')#to get the outline of the number
+guess=input("pick a number!")# picking the first numbre
 from random import randint
 random=(randint(1,number))
 answer= random
-while guess != random:
+while guess != random:# that the guess is not enqual to random
      if guess > random:
-        print "too high"
-        guess=input("pick a number!")
-        guesses=guesses+1
+        print "too high"# if they guess too high
+        guess=input("pick a number!")#they get to pick another number
+        guesses=guesses+1#the guess if tghey get too high is adding one
      else:
-        print 'too low'
-        guess=input("pick a number!")
-        guesses=guesses+1
+        print 'too low'# it will print too low if the number is too low 
+        guess=input("pick a number!")#they get anotther try at picking a numbre
+        guesses=guesses+1# if they geuess too low it will
 print"great you got it!!"
 print "it took you " +str (guesses)+ " tries"
