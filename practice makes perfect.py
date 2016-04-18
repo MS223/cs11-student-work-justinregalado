00c09fc7b6c61647665995d7d3767bbbc33842b6
justinregalado <justin.regalado@ms223.org>
2016-04-18 08:43:27 -0400

    Create practice makes perfect.py

---
+++ practice makes perfect.py
@@ -0,0 +1,16 @@
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+    is_even(3)
+
+# def is_int(x):
+#     if x== int(x)
+#         print True
+#     else:
+#         print False
+    
+# def digit_sum(n):
+#     postive_intergers:
+on this one i need help
