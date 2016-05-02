ff2a27f21b842ccd943e76427e0cb38cbc139d4a
justinregalado <justin.regalado@ms223.org>
2016-05-02 08:45:28 -0400

    Create may 2quiz.py

---
+++ may 2quiz.py
@@ -0,0 +1,21 @@
+# inputwidth=input('how wide')
+# height=input('how tall')
+#
+# for x in range(0,height):
+#     print'* -' * width
+#     print'- *' * width
+
+# number= input("give me a number")
+# max_value= range(1,number+1)
+# print max_value
+
+list1 = [4,5,15,11,23,42]
+list2 = [1,8,7,16,7,35]
+#print list1.index(x)
+def compare_lists(list1 , list2)
+    for x in list1:
+        if x >list2[list1. index(x)]:
+            print x
+        else:
+            print list2[list1.index(x)]
+compare_lists(list1,list2)
