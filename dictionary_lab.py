018b6e2ba321d868835bbb2ca1b6b0a0de60efe2
justinregalado <justin.regalado@ms223.org>
2016-05-16 08:37:21 -0400

    Create dictionary_lab.py

---
+++ dictionary_lab.py
@@ -0,0 +1,18 @@
+user_profile = {
+"name": "Justin Regalado",
+"school": "The Laboratory School of Finance & Technology",
+"birthday": "08/06/98",
+"email": "justin.regalado@ms223.org",
+"username":"king_jrg",
+"number of siblings": "4",
+"skills":"driving,and going fast",
+"about you":"a cars and bike enthusiast with a love for motorsport",
+"favorite quote":"If one day the speed kills me, don't cry. Because I was smiling. Paul Walker",
+"favorite movie": "Bron 2 race",
+"favorite musician": "fab",
+"favorite TV show": "top gear UK and US",
+"favorite book":"i dont have one bro",
+}
+
+print user_profile
+print user_profile ["school"]
