42b1525d1a6f5c88309cac4ccc5269468685ee74
justinregalado <justin.regalado@ms223.org>
2016-05-24 09:29:35 -0400

    Create color wheel.py

---
+++ color wheel.py
@@ -0,0 +1,13 @@
+color clss(object):
+
+pregnancy_test_blue.r=0
+pregnancy_test_blue.g=0
+pregnancy_test_blue.b=205
+
+daddy_run_alway_red.r=178
+daddy_run_alway_red.g=34
+daddy_run_alway_red.b=34
+
+pink_eye_pink.r=
+pink_eye_pink.g
+pink_eye_pink.b
