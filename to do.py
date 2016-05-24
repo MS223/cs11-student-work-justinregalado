279b27324e425e081b7d640117f82286bbca1eeb
justinregalado <justin.regalado@ms223.org>
2016-05-24 08:16:32 -0400

    Update to do.py

--- to do.py
+++ to do.py
@@ -9,18 +9,24 @@ list_dictionary={
     "saturday":[],
     "sunday":[],
 }
-def add():
+def add(action, day):
     list_dictionary[day].append(action)
 
 def get (day):
     print list_dictionary[day]
 
 def choice():
-    user_choice= raw_input("how can i help you")
-    if user_choice == "get":
-        day=raw_input("what day?")
-        get(day)
-    elif user_choice == "add":
-        action=raw_input("what would you like yo add?")
-        add()
+    user_choice=""
+    while user_choice!= "exit":
+        user_choice= raw_input("how can i help you")
+        if user_choice == "get":
+            day=raw_input("what day?")
+            get(day)
+        elif user_choice == "add":
+            action=raw_input("what would you like to add?")
+            day=raw_input("what day")
+            add(action,day)
+
+
 choice()
+
